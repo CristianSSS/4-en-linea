aef1fb2ce88531089ee1476bf7669b92a4bfa0ca
CristianSSS <cristian.salgado4756@gmail.com>
2023-03-10 01:03:10 -0300

    ADD FUNCTIONS

--- clickTablero.js
+++ clickTablero.js
@@ -1,13 +1,57 @@
+// const tabla=document.getElementById("tablero");
+
+// tabla.addEventListener("click",(e)=>{
+//     let element=e.target.closest("#tablero td");
+//     let row= element.parentNode.rowIndex;
+//     let columan= element.cellIndex;
+//     console.log(`fila: ${row}. columna: ${columan} -- ${element}`);
+
+//     //ejemplo (elimianar despues)
+//     let element2=e.target.closest("td div");
+//   element2.setAttribute("class","circle-red");
+
+// });
+
 const tabla=document.getElementById("tablero");
 
 tabla.addEventListener("click",(e)=>{
     let element=e.target.closest("#tablero td");
     let row= element.parentNode.rowIndex;
-    let columan= element.cellIndex;
-    console.log(`fila: ${row}. columna: ${columan} -- ${element}`);
+    let columna= element.cellIndex;
+    
+  
 
-    //ejemplo (elimianar despues)
-    let element2=e.target.closest("td div");
-  element2.setAttribute("class","circle-red");
+    for(let i=tablero.length-1; i>=0;i--){
+      
+      console.log(tablaHtml)
+   
+      if(tablero[i][columna] == 0){
+      
+        tablaHtml[i][columna].setAttribute("class","circle-red");
+        tablero[i][columna]=1;
+        break;
+
+      //     //ejemplo (elimianar despues)
+      // let element2=e.target.closest("td div");
+      // element2.setAttribute("class","circle-red");
+      }
+
+      checkWinner(1);
+
+    }
+
+
+   
+
+
+
+
+
+
+
+
+  
 
 });
+
+
--- createTable.js
+++ createTable.js
@@ -1,25 +1,32 @@
 let tablaHtml=[];
-const table=document.getElementById("tablero");
-let fragment=document.createDocumentFragment();
 
-for(let i=0; i<tablero.length;i++){
-    let tr=document.createElement("tr");
-    let array=[];
-    for(let b=0;b<tablero[0].length;b++){
-        let td=document.createElement("td");
-        let div=document.createElement("div");
-        div.setAttribute("class","circle");
+const createTable=()=>{
 
-        td.appendChild(div);
-        tr.appendChild(td);
+    
+    const table=document.getElementById("tablero");
+    let fragment=document.createDocumentFragment();
 
-        array.push(td);
+    for(let i=0; i<tablero.length;i++){
+        let tr=document.createElement("tr");
+        let array=[];
+        for(let b=0;b<tablero[0].length;b++){
+            let td=document.createElement("td");
+            let div=document.createElement("div");
+            div.setAttribute("class","circle");
+
+            td.appendChild(div);
+            tr.appendChild(td);
+
+            array.push(div);
+        }
+        tablaHtml.push(array)
+        fragment.appendChild(tr);
+
     }
-    tablaHtml.push(array)
-    fragment.appendChild(tr);
+
+    table.appendChild(fragment);
 
 }
 
-table.appendChild(fragment)
 
-console.log(tablaHtml)
+createTable();
